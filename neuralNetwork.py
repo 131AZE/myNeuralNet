29fe0e98d7b05ec081f5cbf9eb50503ea6df77ff
131AZE <kchand23@uic.edu>
2017-09-23 20:24:48 -0500

    Add files via upload

---
+++ neuralNetwork.py
@@ -0,0 +1,53 @@
+import numpy
+import scipy.special
+
+
+class neuralNetwork:
+
+    def __init__(self, inputnodes, hiddennodes,outputnodes,learningrate):
+        #initialising nodes
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        #learning_rates
+        self.lr = learningrate
+
+        #initialising weights
+        self.wih = numpy.random.normal(0.0, pow(self.hnodes,0.5),(self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.onodes,0.5),(self.onodes, self.hnodes))
+
+        #activation function
+        self.activation_function = lambda x: scipy.special.expit(x)
+        pass
+
+    def train(self, inputs_list,targets_list):
+        final_outputs = self.query(inputs_list)
+
+        output_errors = targets - final_outputs
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        self.who += self.lr * numpy.dot( ( output_errors * final_outputs *(1.0 - final_outputs) ) , numpy.transpose(hidden_outputs) )
+        self. wih += self.lr * numpy.dot(( hidden_errors * hidden_outputs *(1.0 - hidden_outputs) ), numpy.transpose(inputs) )
+        pass
+
+    def query(self, inputs_list):
+        inputs = numpy.array(inputs_list,ndmin=2).T
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+        return final_outputs
+
+def __main__():
+    input_nodes = 3
+    hidden_nodes = 3
+    output_nodes = 3
+
+    learning_rate = 0.3
+
+    n = neuralNetwork(input_nodes,hidden_nodes,output_nodes,learning_rate)
+    print(n.query([1.0,0.5,-1.5]))
+
+if __name__ == "__main__":
+    __main__()
